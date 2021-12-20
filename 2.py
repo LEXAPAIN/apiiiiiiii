23fb2381a50327e588784714074863e94c1ffab2
LEXAPAIN <83112474+LEXAPAIN@users.noreply.github.com>
2021-12-20 21:04:12 +0500

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,9 +1,7 @@
 import requests
-
-repos = requests.get('https://api.github.com/search/repositories?q=language:python&sort=stars')
-
-print(f"{repos.json()['total_count']} - Общее количество репозиториев на Python")
-print(f" Самый популярный репозиторий {repos.json()['items'][0]}")
-print("Остальные популярные репозитории")
-for i in repos.json()['items'][1:]:
-    print(i['name'], i['url'])
+repositories = requests.get('https://api.github.com/search/repositories?q=language:python&sort=stars')
+print(f" Общее кол-во репозиториев на Python- {repositories.json()['total_count']} ")
+print(f" Самый популярный репозиторий {repositories.json()['items'][0]}")
+print("Прочие репозитории репозитории:")
+for rep in repositories.json()['items'][1:]:
+    print(rep['name'], rep['url'])
