a3b20053cddb7f1c10a902655611a22dd0deb9b6
LEXAPAIN <83112474+LEXAPAIN@users.noreply.github.com>
2021-12-20 20:53:49 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,12 @@
+import feedparser
 import json
-jsonFile = open("news.json")
-with open("news.json", 'r', encoding='utf-8') as fh:
-    data = json.load(fh)
-print(data)
+
+rss_feed = feedparser.parse('https://lenta.ru/rss')
+result = []
+for news_post in rss_feed['entries']:
+    cv = {'pubDate': news_post['published'], 'title': news_post['title']}
+    result.append(cv)
+
+jsn = json.dumps(result, ensure_ascii=False)
+with open('news.json', 'w', encoding='utf-8') as news_file:
+    news_file.write(jsn)
